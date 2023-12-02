bcc23aeee95e942797c6b8388d973bdbd6c7f4fb
l.galeazzi <l.galeazzi@reply.it>
2023-12-02 14:01:46 +0100

    Fitst Commit

---
+++ src/main/java/com/example/stockandbonds/LinkWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockandbonds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkWorkApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/stockandbonds/configuration/JacksonConfig.java
@@ -0,0 +1,17 @@
+package com.example.stockandbonds.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter(ObjectMapper objectMapper) {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        jsonConverter.setObjectMapper(objectMapper);
+        return jsonConverter;
+    }
+}
