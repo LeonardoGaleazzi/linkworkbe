8a2f7a4945a76fd253d42b7b783e3a81ed30b546
l.galeazzi <l.galeazzi@reply.it>
2023-12-03 12:17:45 +0100

    Finished filter config and controller for resgistration and authentication.

---
+++ src/main/java/com/example/linkwork/auth/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.example.linkwork.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/example/linkwork/auth/AuthenticationResponse.java
@@ -0,0 +1,15 @@
+package com.example.linkwork.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+
+    private  String token;
+}
